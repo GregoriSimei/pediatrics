a9a0de946c74434aa54f2ae90a9cb7e07ff353e0
Gregori Simei <gregorisimei@gmail.com>
2020-04-27 04:35:32 -0300

    doctor informations page created

--- src/main/java/com/pediatrics/app/controller/DoutorController.java
+++ src/main/java/com/pediatrics/app/controller/DoutorController.java
@@ -2,7 +2,9 @@ package com.pediatrics.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.pediatrics.app.model.Doutor;
@@ -13,14 +15,34 @@ import com.pediatrics.app.repository.DoutorRepository;
 public class DoutorController {
 	
 	@Autowired
-	private DoutorRepository DoutorRepository;
+	private DoutorRepository doutorRepository;
 	
 	@RequestMapping
 	public ModelAndView initialPage() {
 		ModelAndView index = new ModelAndView("/doutor/index");
-		Iterable<Doutor> doutores = DoutorRepository.findAll();
+		Iterable<Doutor> doutores = doutorRepository.findAll();
 		index.addObject("doutores",doutores);
 		return index;
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ModelAndView responsavelPage(@PathVariable("id") Long id) {
+		ModelAndView doutorInfoPage = new ModelAndView("/doutor/responsavelInfo");
+		Doutor doutor = doutorRepository.findById(id);
+		doutorInfoPage.addObject("doutor", doutor);
+		return doutorInfoPage;
+	}
+	
+	@RequestMapping(value = "/cadastrar", method = RequestMethod.GET)
+	public ModelAndView formPage(){
+		ModelAndView cadastro = new ModelAndView("/doutor/cadastrar");
+		return cadastro;
+	}
+	
+	@RequestMapping(value = "/cadastrar", method = RequestMethod.POST)
+	public String formPage(Doutor doutor){
+		doutorRepository.save(doutor);
+		return "redirect:/doutor/cadastrar";
 	}
 
 }
